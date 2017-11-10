5873ffa1c4ca14f94613dd77fb65b536afb0f756
陈尧麟 <chenyaolin@megvii.com>
2017-11-10 18:09:20 +0800

    baseline

--- script/clean.sh
+++ script/clean.sh
@@ -1,10 +1,10 @@
 if [ -d script ]
 then
     cd script
-    rm -rf ../data/cache/*
+    # rm -rf ../data/cache/*
     rm -rf ../train_log/*
     cd ..
 else
-    rm -rf ../data/cache/*
+    # rm -rf ../data/cache/*
     rm -rf ../train_log/*
 fi
--- train.py
+++ train.py
@@ -11,10 +11,10 @@ def get_args():
     parse = argparse.ArgumentParser(description='dnn for classification')
     parse.add_argument('-c', '--checkpoint', type=str, default=None)
     parse.add_argument('-e', '--epoch', type=int, default=8000)
-    parse.add_argument('-b', '--batchsize', type=int, default=4096)
-    parse.add_argument('-v', '--valbatchsize', type=int, default=1024)
-    parse.add_argument('-s', '--snapshotstep', type=int, default=1000)
-    parse.add_argument('-r', '--runid', type=str, default='fc_dnn')
+    parse.add_argument('-b', '--batchsize', type=int, default=20000)
+    parse.add_argument('-v', '--valbatchsize', type=int, default=4000)
+    parse.add_argument('-s', '--snapshotstep', type=int, default=100)
+    parse.add_argument('-r', '--runid', type=str, default='dnn')
     parse.add_argument('-lr', '--learningrate', type=float, default=1e-3)
     return parse.parse_args()
 
@@ -26,7 +26,7 @@ validation_set = utils.list_to_array(dp.val[0])
 
 # embed()
 
-net = model.network(checkpoint=args.checkpoint, learning_rate=args.learningrate)
+net = model.network(checkpoint=args.checkpoint, learning_rate=args.learningrate, name=args.runid)
 net.model.fit(
     *train_set, validation_set=validation_set, n_epoch=args.epoch, show_metric=True,
     batch_size=args.batchsize, snapshot_step=args.snapshotstep,

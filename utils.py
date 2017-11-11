a3702748aa0d5911bf853796ea4791097312d0a9
陈尧麟 <chenyaolin@megvii.com>
2017-11-11 17:24:39 +0800

    catastrophic error fix

--- utils.py
+++ utils.py
@@ -1,6 +1,7 @@
 import os, csv, pickle
 import numpy as np
 from IPython import embed
+from config import cfg
 
 def mkdir(path):# {{{
     if not os.path.isdir(path):
@@ -8,7 +9,7 @@ def mkdir(path):# {{{
         return True
     return False
 # }}}
-def load_csv(csv_file):# {{{
+def load_csv(csv_file, shuffle=True):# {{{
     print('[OPR] load_csv %s..' % csv_file)
     data = []
     with open(csv_file, 'r') as f:
@@ -19,21 +20,21 @@ def load_csv(csv_file):# {{{
             each = {'feature': []}
             d = dict(row)
             for key, value in d.items():
-                if 'feature' == key[:7]:
-                    each['feature'].append(value)
-                else:
+                if not 'feature' == key[:7]:
                     each[key] = value
+            for k in range(cfg.input_shape):
+                each['feature'].append(d['feature%s' % k])
             each['feature'] = np.array(each['feature'], dtype=np.float32)
             data.append(each)
             if (i + 1) % 20000 == 0 or i == 0:
                 print('[PRO] done %d..' % (i + 1))
     np.random.seed(233)
-    np.random.shuffle(data)
+    if shuffle:
+        np.random.shuffle(data)
     if 'weight' not in keys:
         weight = None
     else:
         weight = np.array([each['weight'] for each in data], np.float32)
-        weight = weight / np.sum(weight)
     print('[SUC] load_csv done..')
     return data, weight
 # }}}
@@ -52,12 +53,17 @@ def load_cache(path):# {{{
     print('[SUC] load_cache done..')
     return data
 # }}}
-def list_to_array(d):# {{{
+def list_to_array_train(d):# {{{
     feature = np.array([each['feature'] for each in d])
     labels = np.array([each['label'] for each in d], dtype=int)
     n = len(labels)
     res = np.zeros((n, 2))
     res[range(n), labels] = 1
     return feature, res
+# }}}
+def list_to_array_test(d):# {{{
+    feature = np.array([each['feature'] for each in d])
+    index = np.array([each['id'] for each in d], dtype=int)
+    return feature, index
 # }}}
 
